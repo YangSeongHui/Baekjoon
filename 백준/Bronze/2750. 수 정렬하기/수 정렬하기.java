7baff82acda7cdfa7155f597c7c94a1da9c92319
YangSeongHui <95169532+YangSeongHui@users.noreply.github.com>
2022-06-15 21:30:26 +0900

    [Bronze II] Title: 수 정렬하기, Time: 168 ms, Memory: 14948 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+ 
+public class Main {
+   public static void main(String[] args) throws NumberFormatException, IOException {
+		
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i : arr)
+			System.out.println(i);
+   }
+   
+}		
+	
